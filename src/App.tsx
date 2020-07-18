3119528acdf5b4f525afa73c0e63d7114f40305f
ko5 <kotyana1331@gmail.com>
2020-07-18 16:26:53 +0900

    [変更]言語をReact(TS)に変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://cdn.discordapp.com/attachments/700920014475362346/733905175890100384/cotton_circle.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hello World, My name is ko!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://twitter.com/ko_CottonLove"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Twitter
         </a>
       </header>
     </div>
