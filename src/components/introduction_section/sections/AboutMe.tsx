fe9c06eb61a881f85b3394a95e094b8754914dc5
ko5 <kotyana1331@gmail.com>
2021-01-08 22:56:51 +0900

    add: add temporarily sections

---
+++ src/components/introduction_section/sections/AboutMe.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { BaseColors } from 'values/BaseColors'
+import { IntroductionSection } from 'components/introduction_section/IntroductionSection'
+
+
+
+export const AboutMe = () => {
+  return (
+    <IntroductionSection index={1} content={(
+      <>
+
+      </>
+    )} />
+  );
+};
---
+++ src/components/introduction_section/sections/Contacts.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { BaseColors } from 'values/BaseColors'
+import { IntroductionSection } from 'components/introduction_section/IntroductionSection'
+
+
+
+export const Contacts = () => {
+  return (
+    <IntroductionSection index={1} content={(
+      <>
+
+      </>
+    )} />
+  );
+};
