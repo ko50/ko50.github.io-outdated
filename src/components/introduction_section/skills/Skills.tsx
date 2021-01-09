5a2651c3ce03b0addab68b99e7f5d28a6d732fa6
ko5 <kotyana1331@gmail.com>
2021-01-10 01:33:57 +0900

    feat: special animation for skilldata

--- src/components/introduction_section/skills/Skills.tsx
+++ src/components/introduction_section/skills/Skills.tsx
@@ -8,12 +8,20 @@ import { SkillsUnit } from 'components/introduction_section/skills/components/Sk
 import { SkillData, SkillCategory } from 'data/type/SkillData'
 import { SkillValue } from 'data/value/SkillValue'
 
-const Container = styled.div`
+const Container = styled.div<{ visible: Boolean }>`
   display: flex;
   flex-direction: row;
   padding: 30px 40px;
   justify-content: space-around;
+
+  transition: 400ms ease-in;
+  transition-delay: ${(props) => props.visible ? "400" : "800"}ms;
+  opacity: ${(props) => props.visible ? "1" : "0"};
+  transform: translateY(${(props) => props.visible ? "0px" : "6px"});
 `;
+/*display: inline-block;
+  box-sizing: border-box;
+  width: 100%;*/
 
 export const Skills = () => {
   const languagesSkills: SkillData[] =
@@ -27,7 +35,7 @@ export const Skills = () => {
     <TransitionContext.Consumer>
       {currentViewIndex => (
         <IntroductionSection index={1} title="Skills" subTitle="できること" content={(
-          <Container>
+          <Container visible={currentViewIndex === 1}>
             <SkillsUnit category={SkillCategory.language} skills={languagesSkills} />
             <SkillsUnit category={SkillCategory.framework} skills={frameworksSkills} />
             <SkillsUnit category={SkillCategory.tool} skills={toolsSkills} />
