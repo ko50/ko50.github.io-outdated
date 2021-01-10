74aab2221b0e0750e6093258f1fd0bbe5e3b3399
ko5 <kotyana1331@gmail.com>
2021-01-10 18:27:49 +0900

    add: value of works

---
+++ src/data/value/WorkValue.ts
@@ -0,0 +1,10 @@
+import { WorkData } from 'data/type/WorkData'
+
+export const WorkValue: WorkData[] = [
+  {
+    name: "パソコン甲子園 2020",
+    url: "",
+    description: "モバイル部門の本戦に出場しました",
+    tag: ["Flutter", "Dart", "Firebase"],
+  },
+];
