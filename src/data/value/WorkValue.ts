68ce35a62598a7655eee6e5970233a0d9d6975a0
ko5 <kotyana1331@gmail.com>
2021-02-28 02:52:15 +0900

    add: new work data

--- src/data/value/WorkValue.ts
+++ src/data/value/WorkValue.ts
@@ -8,9 +8,15 @@ export const WorkValue: WorkData[] = [
     snapshotSrc: "works_snapshot/.png",
   },*/
   {
-    name: "Portfolio",
+    name: "Portfolio Site (here)",
     url: "https://github.com/ko50/ko50.github.io",
     tag: ["TypeScript", "React"],
     snapshotSrc: "works_snapshot/Portfolio.png",
   },
+  {
+    name: "Reproduced Tetris on Flutter",
+    url: "https://github.com/ko50/re-tetris",
+    tag: ["Dart", "Flutter"],
+    snapshotSrc: "works_snapshot/re_tetris.png",
+  }
 ];
